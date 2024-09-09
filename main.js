1c9610a61fce122b353f39757335b4c17952e150
paolok1 <gpaolok1@gmail.com>
2024-09-09 13:03:06 +0200

    selfwork operatori1

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 15 ;
+let b = 33 ;
+let c = 21 ;
+let d = 11 ;
+let e = 5 ;
+let somma = a + b + c + d + e ;
+console.log(somma);
+console.log(somma / 5) ;
+console.log(' la somma tra i numeri equivale a ' + somma);
+console.log(' la media tra i numeri equivale a 17 ');
+
+
+
+
+
+
+
